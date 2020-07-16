1e7a3f220a0ad59c8bfbb33cb1c6bc33a0983a57
Tardmigg <dmitry.taran333@mail.ru>
2020-07-16 18:06:51 +0300

    qwe

--- start.py
+++ start.py
@@ -53,6 +53,8 @@ class TextView(pygame.sprite.Sprite):
 GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+RANDOM_COLOR = (234, 165, 43)
+
 target_position = [(425, 77), (685, 77), (360, 277), (650, 277)]
 
 FPS = 60
@@ -71,14 +73,12 @@ font1 = pygame.font.Font(None, 50)
 text1 = font1.render("Слово_________слово_________слово слово", 1, WHITE)
 place1 = text1.get_rect(center=(600, 300))
 
+surf_down = pygame.Surface((1200, 100))
+surf_down.fill(RANDOM_COLOR)
+rect_surf_down = pygame.Rect((40, 560, 0, 0))
+
+
 pygame.display.update()
-
-
-def renderView():
-    display.blit(image_surf, image_rect)
-    display.blit(text, place)
-    display.blit(text1, place1)
-
 
 count = 0
 
@@ -92,6 +92,15 @@ text_group = pygame.sprite.Group()
 for value in [["qwe", 100, target_position[0]], ["werfui", 300, target_position[1]], ["earhb", 500, target_position[2]],
               ["it7j", 700, target_position[3]]]:
     TextView(value[0], value[1], 600, value[2], inc_count, text_group)
+
+
+def renderView():
+    display.blit(image_surf, image_rect)
+    display.blit(text, place)
+    display.blit(text1, place1)
+    display.blit(surf_down, rect_surf_down)
+    text_group.draw(display)
+
 
 text_group.draw(display)
 pygame.display.update()
@@ -107,6 +116,5 @@ while True:
             text_group.update((i.pos[0], i.pos[1]), target_position[count])
     text_group.update()
     renderView()
-    text_group.draw(display)
     pygame.display.update()
     clock.tick(FPS)
